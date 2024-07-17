d55fb387499f1ae2b199c23ee0a2a7916cdd831b
ksg-dev <ksg.dev.data@gmail.com>
2024-07-17 15:45:33 -0500

    number guess logic

--- server.py
+++ server.py
@@ -8,19 +8,21 @@ TARGET = random.randint(0, 9)
 @app.route("/<int:number>")
 def check_number(number):
     global TARGET
-    if number == TARGET:
-        return("<h1>You found me!</h1>")
-    elif number < TARGET:
-        return("<h1 >Too low! Try again!</h1>"
+    if number > TARGET:
+        return("<h1 style='color:purple;'>Too high! Try again!</h1>"
+               "<img src='https://media2.giphy.com/media/QyVGDyUsyMEPd7Ktqi/200.webp?cid=ecf05e47n6us6q83oflrs4n3r00rpr8n8la7tdf08x2mgy10&ep=v1_gifs_search&rid=200.webp&ct=g'>")
+    if number < TARGET:
+        return("<h1 style='color:red;'>Too low! Try again!</h1>"
                "<img src='https://media4.giphy.com/media/YmQLj2KxaNz58g7Ofg/giphy.webp?cid=790b76110dwpc02hhugqwrkt7bpljvrlgy63jh88grp8qliz&ep=v1_gifs_search&rid=giphy.webp&ct=g'>")
-    else:
-        pass
+    elif number == TARGET:
+        return ("<h1 style='color:green;'>Great job! You found me!</h1>"
+                "<img src='https://media0.giphy.com/media/5xZoxXPRfSvZsxkD0Y/200.webp?cid=ecf05e47lcb4qos4a5ehwyyfisn2j4zvcuyr8yz4ym1a2a4f&ep=v1_gifs_search&rid=200.webp&ct=g'>")
 
 @app.route("/")
 def hello():
     print(TARGET)
     return ("<h1>Guess a number between 0 and 9</h1>"
-            "<img src='https://media2.giphy.com/media/QyVGDyUsyMEPd7Ktqi/200.webp?cid=ecf05e47n6us6q83oflrs4n3r00rpr8n8la7tdf08x2mgy10&ep=v1_gifs_search&rid=200.webp&ct=g'>")
+            "<img src='https://media0.giphy.com/media/fDUOY00YvlhvtvJIVm/giphy.webp?cid=ecf05e47ss94in3pyi0zdrylrltb5hv4f6rvexqnsx6nbg5u&ep=v1_gifs_search&rid=giphy.webp&ct=g'>")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
