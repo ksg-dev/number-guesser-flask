441ed1f1fd082bb52bfac923e5db26c3b968499f
ksg-dev <ksg.dev.data@gmail.com>
2024-07-17 15:02:02 -0500

    decorator in user class

--- user.py
+++ user.py
@@ -1,11 +1,22 @@
 ## Advanced Python Decorator Functions
 
+
 class User:
     def __init__(self, name):
         self.name = name
         self.is_logged_in = False
 
+def is_authenticated(function):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in == True:
+            function()
+    return wrapper
 
+@is_authenticated
 def create_blog_post(user):
     print(f"This is {user.name}'s new blog post.")
+
+
+new_user = User("Sonni")
+create_blog_post(new_user)
 
