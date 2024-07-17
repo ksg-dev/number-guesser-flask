122f4cc0b86fec400fdf0a31dd75c18eb812893e
ksg-dev <ksg.dev.data@gmail.com>
2024-07-17 14:56:00 -0500

    html formatting decorators

--- main.py
+++ main.py
@@ -4,6 +4,27 @@ app = Flask(__name__)
 
 WEENIE_GIF = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExenFiMDNiZGJ4dmw2d2s3dGI3d250ZW4xdGpzb2Z5NTJjaGF2MzA5NiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/NmGbJwLl7Y4lG/200.webp"
 
+
+def make_bold(function):
+    def wrapper_function():
+        output = function()
+        return f"<b>{output}</b>"
+    return wrapper_function
+
+
+def make_emphasis(function):
+    def wrapper_function():
+        output = function()
+        return f"<em>{output}</em>"
+    return wrapper_function
+
+
+def make_ul(function):
+    def wrapper_function():
+        output = function()
+        return f"<u>{output}</u>"
+    return wrapper_function
+
 @app.route("/")
 def hello_world():
     return "<h1 style='text-align: center'>Hello, World!</h1>" \
@@ -11,9 +32,13 @@ def hello_world():
             "<img src='https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExenFiMDNiZGJ4dmw2d2s3dGI3d250ZW4xdGpzb2Z5NTJjaGF2MzA5NiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/NmGbJwLl7Y4lG/200.webp'>"
 
 # Different routes using app.route decorator
+
 @app.route("/bye")
+@make_bold
+@make_ul
+@make_emphasis
 def say_bye():
-    return "Bye"
+    return "Bye!"
 
 # Creating variable paths and converting path to a specified data type
 # use <> for variables in route
